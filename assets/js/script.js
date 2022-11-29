2b955daaab1ef1cd17db95bdbccdbca9cb1345c3
Robin Ostrowski <104393956+Bin-Ostrowski@users.noreply.github.com>
2022-11-29 13:20:13 -0800

    fizzbuzz algarithm

---
+++ assets/js/script.js
@@ -0,0 +1,15 @@
+// // Fizz Buzz 
+// let i;
+// //define loop arguments
+// for (let i = 1; i <= 100; i++) {
+//     // if num is direvative of three and 5 log fizz buzz
+//     if ((i % 3 == 0) & (i % 5 == 0)) console.log("fizzbuzz");
+//     // if num is dirivative of 5
+//     else if (i % 5 == 0) console.log("buzz");
+//     //if num is dirivative of 3
+//     else if (i % 3 == 0) console.log("fizz");
+//   else {
+//     console.log(i);
+//   }
+// }
+
